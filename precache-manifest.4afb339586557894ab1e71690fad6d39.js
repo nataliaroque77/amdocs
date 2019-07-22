cf62d622d09fc786c17c3dce2585fad0bb20ac42
nroque <natalia.e.roque@gmail.com>
2019-07-22 16:19:01 -0400

    Updates

---
+++ precache-manifest.4afb339586557894ab1e71690fad6d39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc0b0b65285349eee11de48457165bbe",
+    "url": "/nataliaroque77/amdocs/index.html"
+  },
+  {
+    "revision": "ae7eda87e0bcdc6ef96d",
+    "url": "/nataliaroque77/amdocs/static/css/main.8d108ec6.chunk.css"
+  },
+  {
+    "revision": "fd6c47e2e4fc7055df8d",
+    "url": "/nataliaroque77/amdocs/static/js/2.0722f58a.chunk.js"
+  },
+  {
+    "revision": "ae7eda87e0bcdc6ef96d",
+    "url": "/nataliaroque77/amdocs/static/js/main.a37d2404.chunk.js"
+  },
+  {
+    "revision": "b5ac070edf8f54a80e9e",
+    "url": "/nataliaroque77/amdocs/static/js/runtime~main.69bbaf0d.js"
+  }
+]);
