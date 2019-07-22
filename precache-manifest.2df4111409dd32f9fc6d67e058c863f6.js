bcb228ada601881827a4cfef90dab6819796cffd
nroque <natalia.e.roque@gmail.com>
2019-07-22 16:33:57 -0400

    Updates

---
+++ precache-manifest.2df4111409dd32f9fc6d67e058c863f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4935f82b25ff77fa354d645115b9c7e9",
+    "url": "/amdocs/index.html"
+  },
+  {
+    "revision": "ae7eda87e0bcdc6ef96d",
+    "url": "/amdocs/static/css/main.8d108ec6.chunk.css"
+  },
+  {
+    "revision": "fd6c47e2e4fc7055df8d",
+    "url": "/amdocs/static/js/2.0722f58a.chunk.js"
+  },
+  {
+    "revision": "ae7eda87e0bcdc6ef96d",
+    "url": "/amdocs/static/js/main.a37d2404.chunk.js"
+  },
+  {
+    "revision": "1e5497b7d245ea274c8f",
+    "url": "/amdocs/static/js/runtime~main.6c156443.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nataliaroque77/amdocs/precache-manifest.4afb339586557894ab1e71690fad6d39.js"
+  "/amdocs/precache-manifest.2df4111409dd32f9fc6d67e058c863f6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nataliaroque77/amdocs/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/amdocs/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
